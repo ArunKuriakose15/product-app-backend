f4a98e93892c9061ece6a41f62efc54f575777cf
ArunKuriakose15 <arunkp1515@gmail.com>
2024-02-09 00:32:50 +0530

    search produc

--- controllers/productRouter.js
+++ controllers/productRouter.js
@@ -13,5 +13,10 @@ router.get("/view",async(req,res)=>{
     let data=await productModel.find()
     res.json(data)
 })
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await productModel.find(input)
+    res.json(data)
+})
 
 module.exports=router
